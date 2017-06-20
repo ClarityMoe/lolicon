59b1acd765996f908bab645223292a0f36507a47
Capuccino <chinodesuuu@gmail.com>
2017-06-20 04:36:57 +0000

    I did it dad

--- src/loliconException.js
+++
@@ -1,9 +0,0 @@
-
-class loliconException extends Error {
-    constructor(message) {
-        super(message);
-    }
-    loliconException(message) {
-        return Promise.reject(new Error(this.message));
-    }
-}
