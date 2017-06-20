6d238ade83e9cedf22f1325a17d95fe479302cf7
Capuccino <chinodesuuu@gmail.com>
2017-06-20 04:36:41 +0000

    I did it dad

---
+++ src/Lolicon.ts
@@ -0,0 +1,35 @@
+/**
+ *  Lolicon Main
+ * 
+ * Me me big disappointment
+ */
+
+import loliconException from './loliconException';
+import child_process = require('child_process');
+
+export module Lolicon {
+    
+    interface IAttribs {
+        __is_danger : bool,
+        minAge: number,
+        maxAge: 15
+    }
+    
+    class IDoables {
+       public Intice () {
+           //do nothing because weebs are already attracted to underaged anime girls
+           return;
+       }
+       private callTheFBI() {
+            if (this.__is_danger === true ) {
+                child_process.exec('call_911', stdout => {
+                    return stdout;
+                }); 
+            } else {
+                return new loliconException(err);
+            }
+        }
+    }
+}
+
+export module Lolicon;
