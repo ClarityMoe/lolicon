05d074b99f174a91a67bde821ca0bf962847ee58
Dương Tiểu Đồng <38874257+DongDuong9E@users.noreply.github.com>
2020-05-07 11:44:11 +0700

    Bash the lolicon.
    Yes

---
+++ src/lolicon.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+intice () {
+        #Do literally nothing because weebs are already attracted to underage anime girls
+        echo ""
+}
+
+call_the_fbi () {
+        if [[$1 -lt 15]]
+        then
+                call_911
+        else
+                echo "Probably just something weird happened."
+        fi
+}
+
+echo "What's the girl's age? "
+read age
+call_the_fbi $age
