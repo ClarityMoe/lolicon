8e5ad761db98c42f538b9cd905a5ef953a1d4317
E404NNF <37207067+E404NNF@users.noreply.github.com>
2020-08-27 00:33:10 +0200

    Porting to Julia

---
+++ src/lolicon.jl
@@ -0,0 +1,15 @@
+function intice()
+    # Nothing because weebs are already attracted to underage anime girls
+end
+
+function callthefbi(age)
+    if age < 15
+        run(`call_911`)
+    else
+        println("Probably just something weird happened")
+    end
+end
+
+println("What\'s the girl\'s age?")
+age = parse(Int32, readline())
+callthefbi(age)
