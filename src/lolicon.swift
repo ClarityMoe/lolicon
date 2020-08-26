41e04e5aa3007e8bdbdb76b0e1c911e4d8ce2187
Shivam Ramkisoen <s.r.v.ramkisoen@umail.leidenuniv.nl>
2020-08-26 22:20:29 +0200

    Ported Lolicon to Swift by MiraiSubject

---
+++ src/lolicon.swift
@@ -0,0 +1,39 @@
+//
+//  lolicon.swift
+//  lolicon
+//
+//  Created by MiraiSubject on 26/08/2020.
+//
+
+import Foundation
+
+class Loli {
+
+    func intice() {
+        // Nothing because weebs are already attracted to underage anime girls.
+    }
+
+    func call_the_fbi(age: Int) {
+        if (age < 15) {
+            let task = Process()
+            task.launchPath = "call_911"
+            task.launch()
+            task.waitUntilExit()
+        } else {
+            print("Probably just something weird happened.")
+        }
+    }
+}
+
+let loli: Loli = Loli()
+print("What's the girl's age?")
+
+if let age = readLine() {
+    if let intedAge = Int(age) {
+        loli.call_the_fbi(age: intedAge)
+    }
+}
+
+
+
+
